d6a37fff158b918a915d73fde648e4a20e4d6075
Emerson Cruz <emersonpcruz@gmail.com>
2021-11-03 16:20:36 -0300

    Basic static Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/3mers0n" target="_blank" rel="noreferrer">Emerson Cruz</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
